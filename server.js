cfd4fef2bf45bf01593d8611b01a0fbc6e5c5609
Daniel Maldonado <daniel.maldonado@bluealba.com>
2021-02-09 20:33:57 -0300

    added home

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const app = express();
 app.get("/dados", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
 
-const listening = () => console.log("listening to port 3000")
+const listening = () => console.log("listening to port " + process.env.PORT)
 
 app.listen(process.env.PORT, listening)
