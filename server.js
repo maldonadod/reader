240cc82029385c63f725283004f76ea7827c0f66
Daniel Maldonado <daniel.maldonado@bluealba.com>
2021-02-10 01:31:45 -0300

    added socket io

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const express = require("express");
-
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 app.get("/dados", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -10,6 +11,28 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
+const users = new Map()
+
+function listOfUsers(io) {
+  io.emit("list_of_users", {
+    users: Array.from(users.values())
+  });
+}
+
+io.on("connection", socket => {
+  socket.on("new_user", ({name}) => {
+    users.set(socket.id, name)
+    listOfUsers(io)
+  });
+  socket.on("disconnect", () => {
+    users.delete(socket.id)
+    listOfUsers(io)
+  });
+  socket.on("fight", (data) => {
+    console.log(data)
+  });
+});
+
 const listening = () => console.log("listening to port " + process.env.PORT)
 
-app.listen(process.env.PORT, listening)
+server.listen(process.env.PORT, listening)
