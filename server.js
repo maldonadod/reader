2cc761e32ea1a0f515502e29f1506c3da220d3aa
Daniel Maldonado <daniel.maldonado@bluealba.com>
2021-02-12 16:13:09 -0300

    request match, confirm match, show match

--- server.js
+++ server.js
@@ -12,24 +12,53 @@ app.get("/", (req, res) => {
 });
 
 const users = new Map()
+const matches = new Map()
 
 function listOfUsers(io) {
   io.emit("list_of_users", {
-    users: Array.from(users.values())
+    users: Array
+      .from(users.values())
+      .map(user => user.name)
   });
+}
+function players(name) {
+  return Array
+    .from(users.values())
+    .filter(user => user.name === name)
 }
 
 io.on("connection", socket => {
   socket.on("new_user", ({name}) => {
-    users.set(socket.id, name)
+    users.set(socket.id, {id: socket.id, name})
     listOfUsers(io)
   });
   socket.on("disconnect", () => {
     users.delete(socket.id)
     listOfUsers(io)
   });
-  socket.on("fight", (data) => {
-    console.log(data)
+  socket.on("request_new_match", ({ player, player2 }) => {
+    players(player)
+      .map(player => {
+        players(player2)
+          .map(player2 => {
+            matches.set(1, {
+              status: "pending",
+              player,
+              player2
+            })
+            socket
+              .to(player2.id)
+              .emit("request_match");
+          })
+      })
+  });
+  socket.on("match_accepted", () => {
+    const match = matches.get(1)
+    socket
+      .to(match.player.id)
+      .emit("show_match", match);
+    socket
+      .emit("show_match", match);
   });
 });
 
